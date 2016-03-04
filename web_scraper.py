ee09fde77b698cc3057efd0ae136bf904d20537b
Sam Hage <sam@sparkfund.co>
2016-03-03 19:45:53 -0500

    determine number of pages per letter

--- web_scraper.py
+++ web_scraper.py
@@ -11,18 +11,50 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebK
 PREFIX = 'http://crosswordtracker.com/browse/answers-starting-with-'
 SUFFIX = '/?page='
 ANSWER_PAGE = 'http://crosswordtracker.com/answer/'
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def main():
 	"""
 	**********************************************************************************************************************
-
+	Main function to run on program execution
 	"""
+	# scrape_all_data()
+	scrape_single_letter( 'a' )
+
+
+def scrape_all_data():
+	"""
+	**********************************************************************************************************************
+	Goes through all 26 letters to scrape the data
+	"""
+	for letter in ALPHABET:
+		scrape_single_letter( letter )
+
+
+def scrape_single_letter( letter ):
+	"""
+	**********************************************************************************************************************
+	Scrapes data for a single letter
+	"""
+	## first determine how many pages there are ##
+	source = get_source( PREFIX + letter )
+	last_div_index = source.rfind( 'paginator' )
+	last_div_index = source.rfind( 'paginator', 0, last_div_index - 1 )
+	num_pages = source[ last_div_index + 11 : last_div_index + 13 ]
+
+	## iterate over all pages ##
+	for i in range ( 1, num_pages + 1 ):
+		page = PREFIX + letter + SUFFIX + i
+		source = get_source( page )
+
 
 def get_source( page ):
 	"""
 	**********************************************************************************************************************
+	Returns the source code of a page
 
+	@param: {string} page The page URL
 	"""
 	try:
 		## request the resource using boilerplate found on Stack Overflow once upon a time ##
@@ -35,6 +67,14 @@ def get_source( page ):
 		print( 'Resource error' )
 
 	return page_source
+
+
+def get_clues_for_answer( answer, href ):
+	"""
+	**********************************************************************************************************************
+	Given an answer and the href that links to its page, return all corresponding
+	clues
+	"""
 
 
 if __name__ == '__main__':
