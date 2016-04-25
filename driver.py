f3ecd40ae45b267a7571a84b8971bd6a77525773
Sam Hage <sam@sparkfund.co>
2016-04-25 09:45:54 -0400

    refactor code for daily puzzle solving

--- driver.py
+++ driver.py
@@ -31,29 +31,48 @@ def main():
 	**********************************************************************************************************************
 	Run on file execution
 	"""
-	# puzzle_name = WEEK[3]
-	# puzzle_name = 'oct0515'
-	# puzzle_name = 'dec3014'
-	# puzzle_name = 'jan2710'
 
-	## record puzzles done so far ##
-	with open( 'done.txt', 'r' ) as done_file:
-		done = done_file.read().splitlines()
-	done_file.close()
+	args = sys.argv
+	downloads = False
+	allPuzzles = False
+
+	if len( args ) == 1: # no flags
+		pass
+	elif args[1] == '-d' or args[1] == '--downloads':
+		downloads = True
+	elif args[1] == '-a' or args[1] == '--all':
+		allPuzzles = True
+
+	if downloads: # pull latest puzzle from downloads
+		pass
+
+	elif allPuzzles: # test all puzzles
+		## record puzzles done so far ##
+		with open( 'done.txt', 'r' ) as done_file:
+			done = done_file.read().splitlines()
+		done_file.close()
 
 
-	for puzzle_name in os.listdir( 'puzzles1/' ):
+		for puzzle_name in os.listdir( 'puzzles/' ):
 
-		## avoid hidden files and previously solved ones ##
-		if len( puzzle_name ) != 7 or puzzle_name in done:
-			continue
+			## avoid hidden files and previously solved ones ##
+			if len( puzzle_name ) != 7 or puzzle_name in done:
+				continue
+			puzzle_solver.fill( puzzle_name )
+			diff = evaluate.compare_results( puzzle_name )
+			evaluate.score_puzzle( diff )
+			with open( 'done.txt', 'a' ) as done_file:
+				done_file.write( puzzle_name + '\n' )
+			done_file.close()
+
+	else: # do a single puzzle
+		puzzle_name = WEEK[0]
+		# puzzle_name = 'oct0515'
+		# puzzle_name = 'dec3014'
+		# puzzle_name = 'jan2710'
 		puzzle_solver.fill( puzzle_name )
 		diff = evaluate.compare_results( puzzle_name )
 		evaluate.score_puzzle( diff )
-		with open( 'done.txt', 'a' ) as done_file:
-			done_file.write( puzzle_name + '\n' )
-		done_file.close()
-
 
 
 if __name__ == '__main__':
